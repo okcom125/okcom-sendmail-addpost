197ec829f840cb3616935f7d3a046a6cffe8ceea
Yoshinori Sakita <yoshinori.s@ok-computer.jp>
2020-06-05 04:11:55 +0900

    色々と修正

--- okcom-sendmail-addpost.php
+++ okcom-sendmail-addpost.php
@@ -13,14 +13,14 @@ Text Domain: okcom-sendmail-addpost
 add_action( 'admin_menu', 'my_admin_menu' );
 
 function my_admin_menu(){
-	add_utility_page(
+	add_menu_page(
 		__('Sendmail Addpost', 'my-custom-admin'),//（必須）メニューが選択されたとき、ページのタイトルタグに表示されるテキスト。
 		__('Sendmail Addpost', 'my-custom-admin'),//（必須）メニューとして表示されるテキスト
 		'administrator', //（必須） メニューを表示するために必要な権限
 		'my-custom-admin', //（必須） メニューのスラッグ名。一意であり、小文字の英数字、ダッシュ、下線のみを含む必要があります
 		'my_custom_admin', //（オプション） メニューページを表示する際に実行される関数
 		'', //（オプション） メニューのアイコンを示す URL
-		
+
 		2 //（オプション） メニューが表示される位置。省略時はメニュー構造の最下部に表示されます。大きい数値ほど下に表示されます。
 	);
 
@@ -31,9 +31,8 @@ function my_admin_menu(){
 		'manage_options', //ユーザーがこのメニュー表示する際に必要な権限
 		'my-submenu', //既存の WordPress メニューの場合、メニューページコンテンツ表示を処理する PHP ファイル。カスタムトップレベルメニューのサブメニューの場合、このサブメニューページの一意の識別子
 		'my_submenu' //メニューページのコンテンツを表示する関数
-		
-	);
 
+	);
 }
 
 function my_custom_admin(){
@@ -48,15 +47,63 @@ function my_custom_admin(){
 <?php
 }
 
-
 function my_submenu(){
 ?>
 	<div class="wrap">
 		<h2>okcom sendmail addpost</h2>
 
 		<form id="my-submenu-form" method="post" action="">
-			<?php wp_nonce_field( 'my-nonce-key', 'my-submenu' ); ?>
+			<?php wp_nonce_field( 'my-notice-key', 'my-submenu' ); //CSRF対策 ?>
+			<p><?php echo esc_html( __( 'E-Mail Address', 'my-custom-admin' ) ); ?>:
+				<input type="text" name="my-data" value="<?php echo esc_attr( get_option( 'my-data' )); ?>"></p>
+			<p><input type="submit" value="<?php echo esc_attr( __('Save', 'my-custom-admin')); ?>" class="button button-primary buton-large"</p>
 		</form>
 	</div>
+<?php
+}
+
+add_action( 'admin_init', 'my_admin_init' );
+
+function my_admin_init(){
+	if( isset( $_POST['my-submenu'] ) && $_POST['my-submenu'] ){
+		//my-notice-key:アクションの名前　my-submenu:my-submenuの変数をnonce値とする
+		if( check_admin_referer( 'my-notice-key', 'my-submenu') ){
+
+			//エラーオブジェクトを生成することで、後々の入力チェックの結果を追加していくことができる。
+			$e = new WP_Error();
+
+			//保存するための処理
+			if( isset($_POST['my-data']) && $_POST['my-data']) {
+				if( is_email( trim( $_POST['my-data'] ) ) ){ //送信されたデータがEーMailかどうかチェック
+					update_option( 'my-data', $_POST['my-data'] );
+				} else{
+						$e->add(
+							'error',
+							__( 'Please enter a vaild email address.', 'my-custom-admin' )
+						);
+						set_transient( 'my-custom-admin-errors', $e->get_error_messages(), 10 );
+					}
+			} else{
+				update_option( 'my-data', '');
+			}
+			//リダイレクトすることで、ページをリロードした際に再度フォームが送信されるエラーを防ぐ
+			wp_safe_redirect( menu_page_url( 'my-submenu', false) );
+		}
+	}
+}
+
+add_action( 'admin_notices', 'my_admin_notices' );
+
+function my_admin_notices(){
+?>
+	<?php if( $messages = get_transient( 'my-custom-admin-errors' ) ): ?>
+		<div class="error">
+			<ul>
+				<?php foreach( $messages as $message ): ?>
+					<li><?php echo esc_html($message); ?></li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+	<?php endif; ?>
 <?php
 }
